35c5f8de3f8cb00f47a59e3db5ec51ee3029b087
Saurav Raj <saurav.raj5592+git@gmail.com>
2023-02-13 21:48:39 +0530

    Add delete and edit method

--- todo/main/routes.py
+++ todo/main/routes.py
@@ -21,9 +21,27 @@ def add():
 
 
 @main.route('/api/update/<id>', methods=['GET'])
-def update(id):
+def done(id):
+    todo_db = mongo.db.todo
+    todo_db.update_one({"_id": ObjectId(id)}, {"$set": {'complete': True}})
+
+    return redirect(url_for('main.index'))
+
+
+@main.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id):
     todo_db = mongo.db.todo
     todo_item = todo_db.find_one({"_id": ObjectId(id)})
-    todo_item['complete'] = True
-    todo_db.update_one({"_id": ObjectId(id)}, {"$set": {'complete': True}})
+    if (request.method == 'GET'):
+        return render_template('edit_task.html', todo=todo_item)
+    elif (request.method == 'POST'):
+        new_text = request.form.get('edited-todo')
+        todo_db.update_one({"_id": ObjectId(id)}, {"$set": {'text': new_text}})
+        return redirect(url_for('main.index'))
+
+
+@ main.route("/api/delete/<id>", methods=['GET'])
+def delete(id):
+    todo_db = mongo.db.todo
+    todo_db.delete_one({"_id": ObjectId(id)})
     return redirect(url_for('main.index'))
